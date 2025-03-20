530154451869321435a3c87bc494cf8cbe988e41
Kevin Jiang <kevinjiang121@gmail.com>
2025-03-20 15:58:25 -0400

    added channel and change homepage to fit

---
+++ src/app/channel/channel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-channel',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './channel.component.html',
+  styleUrls: ['./channel.component.scss']
+})
+export class ChannelComponent {}
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChannelComponent } from '../channel/channel.component';
 import { ChatWindowComponent } from '../chat-window/chat-window.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule, ChatWindowComponent],
+  imports: [CommonModule, ChatWindowComponent, ChannelComponent],
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
