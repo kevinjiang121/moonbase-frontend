cd3b79fbf9db2914a839406469796b97063b9b1d
Kevin Jiang <kevinjiang121@gmail.com>
2025-03-28 15:59:45 -0400

    added a log off button to home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChannelComponent } from '../channel/channel.component';
 import { ChatWindowComponent } from '../chat-window/chat-window.component';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
@@ -13,7 +15,14 @@ import { ChatWindowComponent } from '../chat-window/chat-window.component';
 export class HomePageComponent {
   messages: string[] = [];
 
+  constructor(private authService: AuthService, private router: Router) {}
+
   onMessageSend(newMessage: string): void {
     this.messages.push(newMessage);
+  }
+
+  logOff(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
   }
 }
